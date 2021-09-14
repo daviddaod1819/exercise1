2afd0dccaa49afcbb897cd0b1eadc078f8d57b16
David <david.corbalan@vanadis.es>
2021-09-14 10:22:35 +0200

    Initial Commit

---
+++ exercise1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise1
+//
+//  Created by David on 14/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
