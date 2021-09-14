91621fcc4467d9d0146e0120573417ef6d5c7845
David <david.corbalan@vanadis.es>
2021-09-14 12:25:52 +0200

    Second part of the exercise (evo-a-amazon)

--- exercise1/ViewController.swift
+++ exercise1/ViewController.swift
@@ -25,8 +25,9 @@ class ViewController: UIViewController {
         Album(title: "Nirvana", artist: "Nevermind", albumImage: UIImage.init(named: "nirvana")!),
         Album(title: "Bob Dylan", artist: "Blonde on Blonde", albumImage: UIImage.init(named: "bobDylan")!),
         Album(title: "Elvis Presley", artist: "Elvis Presley", albumImage: UIImage.init(named: "elvisPresley")!),
-        
     ]
+    
+    let url: String = "https://www.amazon.es/Exile-Main-Vinilo-Rolling-Stones/dp/B003AIEOCY/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=Exile+On+Main+St.&qid=1631614885&sr=8-1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,6 +49,12 @@ class ViewController: UIViewController {
     func fillData() {
         titleLabel.text = "\(albums[index].title) - \(albums[index].artist)"
         frontImageView.image = albums[index].albumImage
+    }
+    
+    @IBAction func openNavigator(_ sender: UIButton) {
+        if let url = URL(string: url) {
+            UIApplication.shared.open(url)
+        }
     }
     
 }
